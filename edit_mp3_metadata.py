d19196122da31cc146437e315dddd7819dec5a92
Marcus Sundell <smulanzak@gmail.com>
2025-05-10 15:56:37 +0200

    Add safety checks to edit_mp3_metadata.py

--- edit_mp3_metadata.py
+++ edit_mp3_metadata.py
@@ -6,20 +6,24 @@ PATH = r''
 
 def edit_metadata(path: str) -> None:
     info = path.split('\\')
-    artist = info[-2]
-    album = info[-1][7:]
-    year = info[-1][1:5]
-    for file in (directory := os.listdir(path)):
-        if file.endswith('.mp3'):
-            tracknumber = str(int(file[:2]))
-            title = file[5:-4]
-            data = EasyID3(f'{path}\\{file}')
-            data["artist"] = artist
-            data["album"] = album
-            data["date"] = year
-            data["title"] = title
-            data["tracknumber"] = f'{tracknumber}/{len(directory)}'
-            data.save()
+    if len(info) >= 2:
+        artist = info[-2]
+        album = info[-1][7:]
+        year = info[-1][1:5]
+        if info[-1].split(' ')[0] == f'({int(year)})':
+            directory = os.listdir(path)
+            number_of_tracks = len([file for file in directory if file.endswith('.mp3')])
+            for file in directory:
+                if file.endswith('.mp3'):
+                    tracknumber = str(int(file[:2]))
+                    title = file[5:-4]
+                    data = EasyID3(f'{path}\\{file}')
+                    data["artist"] = artist
+                    data["album"] = album
+                    data["date"] = year
+                    data["title"] = title
+                    data["tracknumber"] = f'{tracknumber}/{number_of_tracks}'
+                    data.save()
 
 def main() -> None:
     edit_metadata(sys.argv[1] if len(sys.argv) == 2 else PATH)
